7161bd9d8bcea2fb1d2f6fbddda8292dddd1f8d2
hadi <hadi.ygcl@gmail.com>
2022-08-19 11:25:02 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.main')
 
-
 @section('contents')
 
-<h1 class="my-4 text-center">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
 <div class="row justify-content-center">
   <div class="col-md-6">
     <form action="/posts">
@@ -21,20 +20,19 @@
     </form>
   </div>
 </div>
+@if ($posts->count())
+<div class="card mb-3">  
+  <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="..." > 
 
-<div class="card my-4">
+  <div class="card-body text-center">
+    <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+    <p><small class="text-muted">By: <a href="/posts?author={{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none" >{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}</small></p>
+    <p class="card-text">{{ $posts[0]->excerpt }}</p>
+    <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
     
-    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="..." >
-  
-  
-    <div class="card-body text-center">
-      <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
-      <p><small class="text-muted">By: <a href="/posts?author={{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none" >{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}</small></p>
-      <p class="card-text">{{ $posts[0]->excerpt }}</p>
-      <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
-      
-    </div>
-  </div>   
+  </div>
+</div>      
+ 
 
 <div class="container">
     <div class="row">
@@ -43,9 +41,9 @@
             <div class="card mb-3" >
                 <div class="position-absolute bg-dark p-2 " style="background-color: rgba(0, 0, 0, 0.2)">
                    <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a> 
-                </div>               
+                </div>                
                 <img src="https://source.unsplash.com/600x400?{{ $post->category->name }}" class="card-img-top" alt="...">
-                     
+                             
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                   <p><small class="text-muted">By: <a href="/posts?author={{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none" >{{ $post->category->name }}</a> {{ $post->created_at->diffForHumans() }}</small></p>
@@ -57,4 +55,11 @@
     </div>
     @endforeach
 </div>
+@else
+<p class="text-center fs-4">no post found.</p>
+@endif
+<div class="d-flex justify-content-center">
+  {{ $posts->links() }}
+</div>
+    
 @endsection
